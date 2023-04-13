301eb96e72f953d725f3d9e21f9f489337f20c86
theoopsguy <theoopsguy01@gmail.com>
2023-04-13 17:16:26 +0530

    Add line animation

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const changeTurn = () => {
 
 const checkWin = () => {
     const wins = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 0],
+        [1, 4, 7, 5, 15, 0],
+        [2, 5, 8, 15, 15, 0],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
     ]
 
     let boxItems = document.getElementsByClassName("boxItem");
@@ -23,6 +23,8 @@ const checkWin = () => {
             document.getElementsByClassName("info")[0].innerText = boxItems[element[0]].innerText + " Won!";
             gameOver = true;
             document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "12em";
+            document.querySelector('.winningLine').style.width = "20vw";
+            document.querySelector('.winningLine').style.transform = `translate(${element[3]}vw, ${element[4]}vw) rotate(${element[5]}deg)`;
         }
     });
 }
@@ -31,7 +33,7 @@ let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(ele => {
     let boxItem = ele.getElementsByClassName("boxItem")[0];
     ele.addEventListener('click', (e) => {
-        if (boxItem.innerHTML === ''){
+        if (boxItem.innerHTML === '' && !gameOver){
             boxItem.innerText = turn;
             turn = changeTurn();
             checkWin();
@@ -52,4 +54,5 @@ resetBtn.addEventListener('click', () => {
     gameOver = false;
     document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
     document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "0";
+    document.querySelector('.winningLine').style.width = "0vw";
 })
